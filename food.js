6a483748572bbcea0b23b70279630db0820430f1
Apex560-legend <144275462+Apex560-legend@users.noreply.github.com>
2025-07-24 18:28:06 +0100

    Update food.js

--- food.js
+++ food.js
@@ -45,3 +45,31 @@ function checkSlide(e){
 }
 
 window.addEventListener('scroll', debounce(checkSlide));
+
+
+
+
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "fa-solid fa-bars")
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "fa-solid fa-bars");
+})
+
+
+window.onload = function () {
+  // Check if the user has visited before
+  if (!localStorage.getItem("hasVisited")) {
+    alert("Hi There, Welcome to Cherix Eat 😉");
+    localStorage.setItem("hasVisited", "true"); // Mark that the user has visited
+  }
+};
